b26160b9396557479e8327796d533726ccb2dcb9
Piter Bedlechowicz <piter@thewidlarzgroup.com>
2021-01-20 22:38:56 +0100

    Update prettier settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
   singleQuote: true,
   trailingComma: 'all',
-};
+}
--- src/Test.tsx
+++ src/Test.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+import {TouchableOpacity} from 'react-native-gesture-handler'
 import Animated, {
   Easing,
   useAnimatedStyle,
   useSharedValue,
   withSpring,
   withTiming,
-} from 'react-native-reanimated';
-import R from 'ramda';
+} from 'react-native-reanimated'
+import R from 'ramda'
 
 const testOnClick = (value: Animated.SharedValue<number>) => () =>
   R.ifElse(
@@ -18,20 +18,20 @@ const testOnClick = (value: Animated.SharedValue<number>) => () =>
       value.value = withTiming(0, {
         duration: 666,
         easing: Easing.circle,
-      });
+      })
     },
-  )(value);
+  )(value)
 
 const Test = () => {
-  const test = useSharedValue(100);
+  const test = useSharedValue(100)
 
   const testStyle = useAnimatedStyle(() => {
     return {
       backgroundColor: 'red',
       width: test.value,
       height: test.value,
-    };
-  });
+    }
+  })
 
   return (
     <View style={styles.container}>
@@ -40,13 +40,13 @@ const Test = () => {
         <Text>magic here</Text>
       </TouchableOpacity>
     </View>
-  );
-};
+  )
+}
 
-export default Test;
+export default Test
 
 const styles = StyleSheet.create({
   container: {
     marginBottom: 40,
   },
-});
+})

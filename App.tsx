af46a1fe51859574699733d2a0905951a6b5cddb
iceu-bb <adamlaskowski737@gmail.com>
2021-02-26 19:10:58 +0100

    Setup detox for ios and android

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Text } from 'react-native'
 import Main from './src/Main'
 
-const App = () => <Main />
+const App = () => (
+  <>
+    <Text testID="desc-text">Yolololololo DEtox</Text>
+    <Main />
+  </>
+)
 
 export default App

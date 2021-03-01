89d75c46e69064c9af697db9eaee0dcf095479a0
Bart Bajda <53791881+iceu-bb@users.noreply.github.com>
2021-03-01 23:08:23 +0100

    Develop (#9)

--- src/utils/MockedNavigator.tsx
+++ src/utils/MockedNavigator.tsx
@@ -3,7 +3,13 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator()
-const MockedNavigator = ({ component, params = {} }) => (
+const MockedNavigator = ({
+  component,
+  params = {},
+}: {
+  component: React.ComponentType<unknown>
+  params: Partial<unknown>
+}) => (
   <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="MockedScreen" component={component} initialParams={params} />
